a1ee29c45b62706a42f441fc57eb089f9136ce5e
ApiwatKn <122974361+ApiwatKn@users.noreply.github.com>
2023-01-18 22:57:27 +0700

    Add files via upload
    Commanline Apiwat

---
+++ Commanline/Commanlinee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Commanline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Apiwat Daowkoksoong");
+            Console.WriteLine("Student Number: 643450093-9");
+            Console.WriteLine("Email: Apiwat.d@kkumail.com");
+            Console.ReadKey();
+        }
+    }
+}
